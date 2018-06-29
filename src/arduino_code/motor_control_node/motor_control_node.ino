148613a47eab36c101747505e4152788079b31d0
larsenkw <kylew.larsen@gmail.com>
2018-06-28 20:01:56 -0600

    First commit

---
+++ src/arduino_code/motor_control_node/motor_control_node.ino
@@ -0,0 +1,23 @@
+/*
+ * ROS Node for controlling the motor shield on a two-wheeled 
+ * robotics platform.
+ * 
+ * The main node receives input from a Logitech F710 controller and
+ * then processes the values and sends the command signal to this 
+ * Arduino node to control the wheel motors and other hardware.
+ * 
+ * ---Topics---
+ * /
+ */
+
+#include <ros.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
